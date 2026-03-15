b15f43d4c889358f88ef70f4a92e07b51db1025f
Vaijayanthi-R <2k22ece104@kiot.ac.in>
2026-03-15 14:53:13 +0530

    Delete team-ecommerce directory

--- team-ecommerce/backend/src/main/java/com/team_ecommerce/backend/enums/Role.java
+++
@@ -1,8 +0,0 @@
-package com.team_ecommerce.backend.enums;
-//role
-public enum Role {
-    USER,
-    SELLER,
-    ADMIN,
-    SUPER_ADMIN
-}
--- team-ecommerce/backend/src/main/java/com/team_ecommerce/backend/exception/ResourceNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.team_ecommerce.backend.exception;
-//rnf
-public class ResourceNotFoundException {
-    
-}
