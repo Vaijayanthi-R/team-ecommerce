efc4a833968b76f4776a007345437ac13a837c76
dark-sriram <163014346+dark-sriram@users.noreply.github.com>
2026-03-15 23:01:37 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({
+  baseURL: "https://team-ecommerce.onrender.com/api"
+})
 
 api.interceptors.response.use(
   res => res.data,
