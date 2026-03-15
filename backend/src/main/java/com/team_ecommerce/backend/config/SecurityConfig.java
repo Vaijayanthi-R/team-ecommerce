0d40b62ed69ad34ff86688402c258be209d29dec
dark-sriram <163014346+dark-sriram@users.noreply.github.com>
2026-03-15 22:45:10 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/team_ecommerce/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/team_ecommerce/backend/config/SecurityConfig.java
@@ -76,7 +76,6 @@ public class SecurityConfig {
         config.setAllowCredentials(true);
  
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.setAllowCredentials(true);
         config.addAllowedOrigin("https://team-ecommerce-alpha.vercel.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
